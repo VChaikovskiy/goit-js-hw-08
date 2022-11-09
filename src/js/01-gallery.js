e988600988446d7d91c16e89c1e9ec62102edc5d
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-11-09 23:50:46 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
 
 console.log(galleryItems);
+
+// function createGalleryMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>`;
+//     })
+//     .join('');
+// }
+
+// const addGalleryMarkup = createGalleryMarkup(galleryItems);
+// galleryRef.insertAdjacentHTML('afterbegin', addGalleryMarkup);
+
+// galleryRef.addEventListener('click', onImgClick);
+
+// const ligthbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
+
+// function onImgClick(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+// }

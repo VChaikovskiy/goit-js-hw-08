31ab672cc9c388aebcb60d2e7a763df0cd4e8d09
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-11-10 20:56:21 +0200

    update 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,29 +7,29 @@ const galleryRef = document.querySelector('.gallery');
 
 console.log(galleryItems);
 
-// function createGalleryMarkup(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `<a class="gallery__item" href="${original}">
-//   <img class="gallery__image" src="${preview}" alt="${description}" />
-// </a>`;
-//     })
-//     .join('');
-// }
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
 
-// const addGalleryMarkup = createGalleryMarkup(galleryItems);
-// galleryRef.insertAdjacentHTML('afterbegin', addGalleryMarkup);
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML('afterbegin', addGalleryMarkup);
 
-// galleryRef.addEventListener('click', onImgClick);
+galleryRef.addEventListener('click', onImgClick);
 
-// const ligthbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
+const ligthbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-// function onImgClick(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-// }
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}

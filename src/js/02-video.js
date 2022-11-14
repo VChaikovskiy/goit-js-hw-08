c084ef7e2b4487cec768c0292f6a23c311fe6551
VChaikovskiy <ch.v.s.p.i@gmail.com>
2022-11-14 11:20:45 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
